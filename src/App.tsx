53504f51bc9638bcfdaea1927dbdc29af7bd8aff
Andrii Halai <halaiandriy@gmail.com>
2024-11-28 23:27:45 +0200

    Fix token

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import styles from './App.module.css';
 import { Sidebar } from './components/Sidebar/Sidebar';
 import { useAuth } from './stores/auth';
+import { useEffect } from 'react';
 
 export const App = () => {
-  const { isAuth } = useAuth();
+  const { isAuth, login } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      login(token);
+    } else {
+      navigate('/login', { replace: true });
+    }
+  }, [isAuth]);
+
   return (
     <div className={styles['app-container']}>
-      {isAuth ? (
+      {!isAuth ? (
         <>
           <Sidebar />
           <main className={styles['outlined-container']}>
