72e8ace953466f52da4c234a450db7ba9b406b42
Andrii Halai <halaiandriy@gmail.com>
2024-11-28 22:42:55 +0200

    Fix sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Outlet } from 'react-router-dom';
 import styles from './App.module.css';
 import { Sidebar } from './components/Sidebar/Sidebar';
+import { useAuth } from './stores/auth';
 
 export const App = () => {
+  const { isAuth } = useAuth();
   return (
     <div className={styles['app-container']}>
-      <Sidebar />
-      <main className={styles['outlined-container']}>
+      {isAuth ? (
+        <>
+          <Sidebar />
+          <main className={styles['outlined-container']}>
+            <Outlet />
+          </main>
+        </>
+      ) : (
         <Outlet />
-      </main>
+      )}
     </div>
   );
 };
